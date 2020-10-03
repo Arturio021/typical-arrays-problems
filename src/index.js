fa989a7ecb37ac7539d0afe3169c5b91b6462fd1
Artur <korbut.arturio@gmail.com>
2020-10-03 18:03:32 +0300

    feat: add minArray function

--- src/index.js
+++ src/index.js
@@ -11,30 +11,3 @@ exports.min = function min(array) {
         return 0;
     }
 };
-
-exports.max = function max(array) {
-    if (array && array.length > 0) {
-        maxArr = array[0];
-        for (var i = 0; i < array.length; i++) {
-            if (array[i] > maxArr) {
-                maxArr = array[i];
-            }
-        }
-        return maxArr;
-    } else {
-        return 0;
-    }
-};
-
-exports.avg = function avg(array) {
-    if (array && array.length > 0) {
-        var sum = 0;
-        for (var i = 0; i < array.length; i++) {
-            sum += array[i];
-        }
-        var result = sum / array.length;
-        return result;
-    } else {
-        return 0;
-    }
-};
